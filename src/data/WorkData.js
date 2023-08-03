12ddf7eb805f7c312e1d3c609916a8d46cf66c8a
Kanishk Singh Maurya <110882432+KanishkSingh3955@users.noreply.github.com>
2023-08-03 16:34:35 +0530

    Add files via upload

---
+++ src/data/WorkData.js
@@ -0,0 +1,43 @@
+export const Work = [
+    {
+        id:1,
+        name:"Movie WebApp",
+        description:"It is build on top of the React JS,Redux with Sass and GSAP for smooth scrolling animations.",
+        tags:["react","gsap","redux"],
+        
+        demo:"https://moviex-39.netlify.app/",
+        github:"https://github.com/KanishkSingh3955/Movie-WebAPP"
+    },
+    {
+        id:2,
+        name:"E-commerce",
+        description:"A Ecommerce project  build in ReactJS. With searching and filtering functionalities add-to-cart functionality, user       authentication,.",
+        tags:["react","sass"],
+        
+        demo:"https://we-dev-store.netlify.app/",
+        github:"https://github.com/KanishkSingh3955/E-commerce"
+        
+    },
+    {
+        id:3,
+        name:"Youtube-Clone",
+        description:"A Youtube CLone project  build in ReactJS. With searching and filtering functionalities , ,.",
+        tags:["react","sass"],
+        
+        demo:"https://ksm-youtube-clone.netlify.app",
+        github:""
+        
+    },
+    {
+        id:4,
+        name:"Algorithm -Visulaiser",
+        description:"A Algorithm Visulaise project  build in HTml , CSS, Js. Where you can see how algorithm is work internally , ,.",
+        tags:["react","sass"],
+        
+        demo:"https://algorithm-visualsier.netlify.app",
+        github:""
+        
+    },
+    
+  
+] 
